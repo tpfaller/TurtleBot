d0c3adf4bdca1767c04643ddaab699d70298c899
Quirin Rothmeier <quirin.rothmeier@gmx.de>
2022-05-20 21:27:39 +0200

    JSON Integration + Icons auf Canvas

--- uxd/src/components/GameCanvas/GameCanvas.js
+++ uxd/src/components/GameCanvas/GameCanvas.js
@@ -1,37 +1,42 @@
-import constants from "./constants";
-import { onMounted } from 'vue'
-
 export default {
     name: "SnakeCanvas",
     props: {
         cellSize: Number,
-        boardSize: Array,
         speed: Number,
         isPlaying: Boolean,
-        stop: Function,
         scores: Number,
-        obstacles: Array
+        positionData: Array
     },
     computed: {
         boardSizePxX(axis) {
-            return this.cellSize * this.boardSize[0];
+            return this.cellSize * this.positionData["Spielfeld"][0];
         },
         boardSizePxY() {
-            return this.cellSize * this.boardSize[1];
+            return this.cellSize * this.positionData["Spielfeld"][1];
+        }
+    },
+    data() {
+        return {
+            bot: this.positionData["Objekte"]["TurtleBot"],
+            bot_image: Image,
+            heroes: [this.positionData["Objekte"]["Captain_America"], this.positionData["Objekte"]["Iron_Man"], this.positionData["Objekte"]["Hulk"]],
+            heroes_names: ["Captain_America", "Iron_Man", "Hulk"],
+            heroes_images: Image
         }
     },
     mounted() {
         this.boardContext = this.$refs.board.getContext("2d");
         window.addEventListener("keydown", this.onKeyPress);
         this.clear();
-        this.resetSnake();
-        this.move();
-        this.setupObstacles();
+        this.reset();
         this.setupHeroes();
-        console.log(this.obstacles);
+        this.setupBot();
+        this.move();
+
+        console.log(this.positionData);
     },
     created() {
-        this.resetSnake();
+        this.reset();
     },
     beforeDestroy() {
         window.removeEventListener("keydown", this.onKeyPress);
@@ -40,31 +45,28 @@ export default {
         isPlaying(value) {
             this.clear();
             if (value) {
-                this.resetSnake();
+                this.reset();
                 this.move();
             }
+        },
+        positionData() {
+            console.log(this.positionData["Spielfeld"]);
         }
     },
     methods: {
-        resetSnake() {
-            this.snake = [
-                {
-                    x: this.getMiddleCellX(),
-                    y: this.getMiddleCellY()
-                }
-            ];
-            const randomDirectionIndex = Math.floor(Math.random() * 4);
-            this.direction = constants[randomDirectionIndex];
-            this.targetCell = null;
+        reset() {
+            //TODO
         },
-        setupObstacles() {
-            this.obstacles.forEach(obstacle => {
+        drawObstacles() {
+            var obstacles = [this.positionData["Objekte"]["Hindernis_1"], this.positionData["Objekte"]["Hindernis_2"], this.positionData["Objekte"]["Hindernis_3"]];
+
+            obstacles.forEach(obstacle => {
                 this.boardContext.beginPath();
                 this.boardContext.rect(
                     this.cellSize + obstacle[0],
                     this.cellSize + obstacle[1],
-                    this.cellSize + 100,
-                    this.cellSize + 100
+                    this.cellSize + obstacle[2],
+                    this.cellSize + obstacle[3]
                 );
                 this.boardContext.fillStyle = "red";
                 this.boardContext.fill();
@@ -72,20 +74,55 @@ export default {
             });
         },
         setupHeroes() {
-            let p = new Path2D('M10 10 h 180 v 80 h -80 Z');
-            this.boardContext.translate(100, 100);
-            this.boardContext.fill(p);
-            var image = new Image();
-            image.onload = function () {
-                this.boardContext.drawImage(this, 0, 0);
-            }
-            image.src = "../src/assets/powerranger.svg";
+            this.heroes.forEach(function (hero, i) {
+
+                var image = document.createElement('img');
+                image.onload = () => this.drawHeroes();
+
+                image.src = "../src/assets/images/" + this.heroes_names[i] + ".svg";
+
+                this.heroes_images[i] = image;
+            }, this);
         },
-        getMiddleCellX() {
-            return Math.round(this.boardSize[0] / 2);
+        drawHeroes() {
+            this.heroes.forEach(hero => {
+                this.boardContext.beginPath();
+                this.boardContext.rect(
+                    this.cellSize + hero[0],
+                    this.cellSize + hero[1],
+                    this.cellSize + hero[2],
+                    this.cellSize + hero[3]
+                );
+                this.boardContext.fillStyle = "yellow";
+                this.boardContext.fill();
+                this.boardContext.closePath();
+            }, this);
+
+            this.heroes.forEach(function (hero, i) {
+                this.boardContext.drawImage(this.heroes_images[i], hero[0], hero[1], hero[2], hero[3]);
+            }, this);
         },
-        getMiddleCellY() {
-            return Math.round(this.boardSize[1] / 2);
+        setupBot() {
+            var image = document.createElement('img');
+            image.onload = () => this.drawBot();
+
+            image.src = "../src/assets/images/Bot.svg";
+
+            this.bot_image = image;
+        },
+        drawBot() {
+            this.boardContext.beginPath();
+            this.boardContext.rect(
+                this.cellSize + this.bot[0],
+                this.cellSize + this.bot[1],
+                this.cellSize + this.bot[2],
+                this.cellSize + this.bot[3]
+            );
+            this.boardContext.fillStyle = "yellow";
+            this.boardContext.fill();
+            this.boardContext.closePath();
+
+            this.boardContext.drawImage(this.bot_image, this.bot[0], this.bot[1], this.bot[2], this.bot[3]);
         },
         move() {
             if (!this.isPlaying) {
@@ -93,98 +130,23 @@ export default {
             }
 
             this.clear();
-            this.setupObstacles();
-            //this.setTargetCell();
-
-            const newHeadCell = {
-                x: this.snake[0].x + this.direction.move.x,
-                y: this.snake[0].y + this.direction.move.y
-            };
-
-            if (
-                this.isCellOutOfBoard(newHeadCell) ||
-                this.amountCellsInSnake(this.snake[0]) > 1
-            ) {
-                this.stop();
-                alert(`Game over! You've scored ${this.scores} points.`);
-            }
-
-            /*
-            if (this.isTargetNewHead()) {
-                this.snake.unshift(this.targetCell);
-                //this.targetCell = null;
-            } else {
-                this.snake.unshift(newHeadCell);
-                this.snake.pop();
-            }*/
-
-            this.snake.unshift(newHeadCell);
-            this.snake.pop();
-
-            this.boardContext.beginPath();
-            this.snake.forEach(this.drawCell);
-            this.boardContext.closePath();
+            this.drawObstacles();
+            this.drawHeroes();
+            this.drawBot();
         },
         clear() {
             this.boardContext.clearRect(0, 0, this.boardSizePxX, this.boardSizePxY);
         },
-        drawCell({ x, y }) {
-            this.boardContext.rect(
-                x * this.cellSize + 100,
-                y * this.cellSize + 100,
-                this.cellSize + 100,
-                this.cellSize + 100
-            );
-            this.boardContext.fillStyle = "black";
-            this.boardContext.fill();
-        },
-        isCellOutOfBoard({ x, y }) {
-            return x < 0 || y < 0 || x >= this.boardSize[0] || y >= this.boardSize[1];
-        },
         onKeyPress(event) {
-            const newDirection = constants.find(c => c.keyCode === event.keyCode);
-            if (!newDirection) {
-                return;
-            }
-            this.direction = newDirection;
-            this.move();
-        },
-        /*
-        setTargetCell() {
-            if (!this.targetCell) {
-                let targetCell = this.getRandomCell();
-                while (this.amountCellsInSnake(targetCell) > 0) {
-                    targetCell = this.getRandomCell;
-                }
-                this.targetCell = targetCell;
+
+            switch(event.keyCode){
+                case 37: this.bot[0] -= 10; break;
+                case 38: this.bot[1] -= 10; break;
+                case 39: this.bot[0] += 10; break;
+                case 40: this.bot[1] += 10; break;
             }
 
-            this.boardContext.beginPath();
-            this.boardContext.rect(
-                this.targetCell.x * this.cellSize,
-                this.targetCell.y * this.cellSize,
-                this.cellSize,
-                this.cellSize
-            );
-            this.boardContext.fillStyle = "red";
-            this.boardContext.fill();
-            this.boardContext.closePath();
-        },
-        getRandomCell() {
-            return {
-                x: Math.floor(Math.random() * this.boardSizeX),
-                y: Math.floor(Math.random() * this.boardSizeY)
-            };
-        },*/
-        amountCellsInSnake(cell) {
-            return this.snake.filter(({ x, y }) => x === cell.x && y === cell.y)
-                .length;
-        },
-        isTargetNewHead() {
-            return (
-                this.snake[0].x + this.direction.move.x === this.targetCell.x &&
-                this.snake[0].y + this.direction.move.y === this.targetCell.y
-            );
+            this.move();
         }
     }
 };
--- uxd/src/views/Game.vue
+++ uxd/src/views/Game.vue
@@ -5,22 +5,24 @@ import gameturtlebot from "../assets/gameturtlebot";
 <template>
     <section class="content game">
         <div id="game-interaction">
-            <GameCanvas :cellSize="cellSize" :boardSize="boardSize" :speed="speed" :isPlaying="isPlaying" :stop="stop"
-                :scores="scores" :obstacles="obstacles" />
+            <GameCanvas :cellSize="cellSize" :speed="speed" :isPlaying="isPlaying"
+                :scores="scores" :positionData="positionData" />
         </div>
         <div id="game-instructions">
             <h1 class="title">{{ $t(gameturtlebot[currentStep].title) }}</h1>
             <p class="description">{{ $t(gameturtlebot[currentStep].description) }}</p>
             <img :src="gameturtlebot[currentStep].img">
-            <button :class="{ disabled: buttonDisabled, loading: buttonLoading }" :disabled=buttonDisabled @click="nextStep">{{
-                    $t(gameturtlebot[currentStep].action)
-            }}</button>
+            <button :class="{ disabled: buttonDisabled, loading: buttonLoading }" :disabled=buttonDisabled
+                @click="nextStep">{{
+                        $t(gameturtlebot[currentStep].action)
+                }}</button>
         </div>
     </section>
 </template>
 
 <script>
 import GameCanvas from '../components/GameCanvas/GameCanvas.vue';
+import PosData from '../components/GameCanvas/exampleinput.json'
 
 export default {
     name: "Game",
@@ -30,18 +32,18 @@ export default {
     data() {
         return {
             cellSize: 1,
-            boardSize: [1920, 1080],
             speed: 10,
-            scores: 0,
             isPlaying: true,
-            obstacles: [[0, 0], [100, 150], [1820, 980]],
+            scores: 0,
             currentStep: 0,
             buttonDisabled: false,
-            buttonLoading: false
+            buttonLoading: false,
+            positionData: JSON.parse(JSON.stringify(PosData))
         };
     },
     methods: {
         nextStep() {
+            this.setPositionData();
             if (this.currentStep + 1 == 1) {
                 this.currentStep++;
                 this.detectObstacles();
@@ -60,9 +62,12 @@ export default {
         drawCoins() {
             setTimeout(() => this.setOrder(), 2000);
         },
-        setOrder(){
+        setOrder() {
             this.buttonLoading = false;
             this.currentStep++;
+        },
+        setPositionData() {
+            this.positionData = JSON.parse(JSON.stringify(PosData));
         }
     }
 };
