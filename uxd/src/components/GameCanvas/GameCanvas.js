8b74721fb88e5075ac0d0c10fa1debd73023d302
Quirin Rothmeier <quirin.rothmeier@gmx.de>
2022-05-21 15:05:11 +0200

    Münzen generieren + Collision Detection

--- uxd/src/components/GameCanvas/GameCanvas.js
+++ uxd/src/components/GameCanvas/GameCanvas.js
@@ -21,7 +21,10 @@ export default {
             bot_image: Image,
             heroes: [this.positionData["Objekte"]["Captain_America"], this.positionData["Objekte"]["Iron_Man"], this.positionData["Objekte"]["Hulk"]],
             heroes_names: ["Captain_America", "Iron_Man", "Hulk"],
-            heroes_images: Image
+            heroes_images: Image,
+            coins: [],
+            coins_image: Image,
+            coins_collected: 0
         }
     },
     mounted() {
@@ -31,6 +34,8 @@ export default {
         this.reset();
         this.setupHeroes();
         this.setupBot();
+        this.generateCoins();
+        this.setupCoins();
         this.move();
 
         console.log(this.positionData);
@@ -68,7 +73,7 @@ export default {
                     this.cellSize + obstacle[2],
                     this.cellSize + obstacle[3]
                 );
-                this.boardContext.fillStyle = "red";
+                this.boardContext.fillStyle = "black";
                 this.boardContext.fill();
                 this.boardContext.closePath();
             });
@@ -123,6 +128,52 @@ export default {
             this.boardContext.closePath();
 
             this.boardContext.drawImage(this.bot_image, this.bot[0], this.bot[1], this.bot[2], this.bot[3]);
+            this.checkCoinCollision();
+        },
+        checkCoinCollision(){
+            this.coins.forEach(coin => {
+                if(coin[2] == true) return;
+                if(this.bot[0] + this.bot[2] >= coin[0] && this.bot[0] <= coin[0] + 20 && this.bot[1] + this.bot[3] >= coin[1] && this.bot[1] <= coin[1]){
+                    coin[2] = true;
+                    this.coins_collected++;
+                    console.log("Collected coins: " + this.coins_collected);
+                }
+            }, this);
+        },
+        generateCoins(){
+            var generatedCoins = 0;
+            while(generatedCoins < 10){
+                let randomX = Math.floor(Math.random() * (this.positionData["Spielfeld"][0] - 20));
+                let randomY = Math.floor(Math.random() * (this.positionData["Spielfeld"][1] - 20));
+                let generatedCoin = [randomX, randomY, false];
+                //TODO: check if generatedCoin collides
+                this.coins.push(generatedCoin);
+                generatedCoins++;
+            }
+            console.log("Generated coins: " + this.coins);
+        },
+        setupCoins(){
+            var image = document.createElement('img');
+            image.onload = () => this.drawCoins();
+
+            image.src = "../src/assets/images/Bot.svg";
+
+            this.coins_image = image;
+        },
+        drawCoins(){
+            this.coins.forEach(coin => {
+                if(coin[2] == true) return;
+                this.boardContext.beginPath();
+                this.boardContext.rect(
+                    this.cellSize + coin[0],
+                    this.cellSize + coin[1],
+                    this.cellSize + 20,
+                    this.cellSize + 20
+                );
+                this.boardContext.fillStyle = "red";
+                this.boardContext.fill();
+                this.boardContext.closePath();
+            }, this);
         },
         move() {
             if (!this.isPlaying) {
@@ -133,6 +184,7 @@ export default {
             this.drawObstacles();
             this.drawHeroes();
             this.drawBot();
+            this.drawCoins();
         },
         clear() {
             this.boardContext.clearRect(0, 0, this.boardSizePxX, this.boardSizePxY);
