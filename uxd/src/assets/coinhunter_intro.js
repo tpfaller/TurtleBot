4984a7aef59192e92022e2496bbedb81aac3eb3f
Quirin Rothmeier <quirin.rothmeier@gmx.de>
2022-06-03 22:17:19 +0200

    Lottie Animations

--- uxd/src/assets/coinhunter_intro.js
+++ uxd/src/assets/coinhunter_intro.js
@@ -1,16 +1,18 @@
+import animationData1 from '../../src/assets/lottiefiles/tutorial_1.json'
+import animationData2 from '../../src/assets/lottiefiles/tutorial_2.json'
 export default [
   {
-    "src": "/src/assets/images/Home.svg",
+    "src": animationData1,
     "title": "coinhunter.intro.title-1",
     "content": "coinhunter.intro.description-1"
   },
   {
-    "src": "/src/assets/images/Home.svg",
+    "src": animationData1,
     "title": "coinhunter.intro.title-2",
     "content": "coinhunter.intro.description-2"
   },
   {
-    "src": "/src/assets/images/Home.svg",
+    "src": animationData2,
     "title": "coinhunter.intro.title-3",
     "content": "coinhunter.intro.description-3"
   }
--- uxd/src/views/Intro.vue
+++ uxd/src/views/Intro.vue
@@ -7,7 +7,7 @@ import coinhunter_intro from "../assets/coinhunter_intro";
         <carousel :items-to-show="1">
             <slide v-for="item in coinhunter_intro" :key="item">
                 <div class="carousel__item">
-                    <img :src="item.src">
+                    <lottie-animation ref="anim" :loop="true" :animationData="item.src" />
                     <div>
                         <h3 class="title">{{ $t(item.title) }}</h3>
                         <p>{{ $t(item.content) }}</p>
@@ -47,41 +47,50 @@ export default {
 
 <style>
 .carousel__item {
-  min-height: 200px;
-  width: 100%;
-  color: var(--text-color);
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  padding: 2rem;
+    min-height: 200px;
+    width: 100%;
+    color: var(--text-color);
+    display: flex;
+    flex-direction: column;
+    /*gap: 2rem;*/
+    padding: 2rem;
 }
 
-.carousel__item > img{
+.carousel__item>img {
     height: 30vh;
     object-fit: contain;
 }
 
-.carousel__item > *{
+.carousel__item>* {
     pointer-events: none;
     user-select: none;
 }
 
 .carousel__prev--in-active,
 .carousel__next--in-active {
-  display: none;
+    display: none;
 }
 
-.carousel__prev, .carousel__next{
+.carousel__prev,
+.carousel__next {
     background-color: var(--text-color-primary);
 }
 
-.carousel__viewport{
+.carousel__viewport {
     border-radius: 2rem;
     background-color: var(--white);
     box-shadow: 0px 0px 0px 1px var(--border-color);
 }
 
-.carousel__slide{
+.carousel__slide {
     align-items: flex-start;
+}
+
+.intro svg{
+    max-height: 30vh;
+}
+
+.intro svg > g{
+    transform: translate(-90px, -110px)scale(1.15);
 }
 </style>
