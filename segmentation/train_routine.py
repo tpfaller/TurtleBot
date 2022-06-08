e6d0e691adb1350696c6560fb0a6449eef4d7588
tpfaller <tpfaller95@protonmail.com>
2022-06-08 20:57:53 +0200

    FP16-Scaler and weighted loss implemented

--- segmentation/train_routine.py
+++ segmentation/train_routine.py
@@ -1,6 +1,7 @@
 import argparse
+import math
 import os
-import torch
+import sys
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from augmentations import *
@@ -29,16 +30,15 @@ def IoU(preds, targets) -> float:
     return torch.mean(IoU).item()
 
 
-def eval_one_epoch(model, loader, device, writer: SummaryWriter, epoch) -> dict:
+def eval_one_epoch(model, criterion, loader, device, writer: SummaryWriter, epoch) -> dict:
     model.eval()
     show_pred_flag, epoch_iou, epoch_loss = True, .0, .0
-    criterion = torch.nn.BCEWithLogitsLoss()
     for ix, (images, targets) in enumerate(loader):
         images, targets = images.to(device), targets.to(device)
         output = model(images)['out']
         epoch_loss += criterion(output, targets).item()
         output = output.detach().cpu()
-        idx = torch.argmax(output, dim=1, keepdims=True)
+        idx = torch.argmax(output, dim=1, keepdim=True)
         preds = torch.zeros_like(output).scatter_(1, idx, 1.)
         epoch_iou += IoU(preds, targets.detach().cpu())
         if show_pred_flag:
@@ -52,27 +52,42 @@ def eval_one_epoch(model, loader, device, writer: SummaryWriter, epoch) -> dict:
     return {'Epoch_Loss': epoch_loss, 'Epoch_IoU': epoch_iou}
 
 
-def train_one_epoch(model, optimizer, scheduler, loader, device, writer, epoch) -> None:
+def train_one_epoch(model, criterion, optimizer, scheduler, loader, fp16_scaler, device, writer, epoch) -> None:
     model.train()
-    criterion = torch.nn.BCEWithLogitsLoss()
     epoch_loss, epoch_iou = .0, .0
     for ix, (images, targets) in enumerate(loader):
         images = images.to(device)
         targets = targets.to(device)
 
+        with torch.cuda.amp.autocast(fp16_scaler is not None):
+            # optimizer.zero_grad()
+            output = model(images)['out']
+            loss = criterion(output, targets)
+
+        if not math.isfinite(loss.item()):
+            print("Loss is {}, stopping training".format(loss.item()))
+            sys.exit(1)
+
+        # student update
         optimizer.zero_grad()
-        output = model(images)['out']
-        loss = criterion(output, targets)
+        param_norms = None
+        if fp16_scaler is None:
+            loss.backward()
+            optimizer.step()
+        else:
+            fp16_scaler.scale(loss).backward()
+            fp16_scaler.step(optimizer)
+            fp16_scaler.update()
 
         epoch_loss += loss.item()
         output = output.detach().cpu()
-        idx = torch.argmax(output, dim=1, keepdims=True)
+        idx = torch.argmax(output, dim=1, keepdim=True)
 
         preds = torch.zeros_like(output).scatter_(1, idx, 1.)
         epoch_iou += IoU(preds, targets.detach().cpu())
 
-        loss.backward()
-        optimizer.step()
+        # loss.backward()
+        # optimizer.step()
     epoch_loss /= len(loader)
     epoch_iou /= len(loader)
     print('IoU: ', epoch_iou, '\nEpoch Loss: ', epoch_loss)
@@ -81,12 +96,12 @@ def train_one_epoch(model, optimizer, scheduler, loader, device, writer, epoch) 
     scheduler.step()
 
 
-
 def train_model(args) -> None:
     ckp_dir = create_dir(args.ckp_dir, args.arch)
     writer = SummaryWriter(log_dir=os.path.join(ckp_dir, 'runs'))
-    transform_train = CustomCompose([ToTensor(), Resize(400, 400), Jitter(), HorizontalFlip(), Normalize()])
-    transform_val = CustomCompose([ToTensor(), Resize(400, 400), Normalize()])
+    width, height = 400, 400
+    transform_train = CustomCompose([ToTensor(), Resize(width, height), Jitter(), HorizontalFlip(), Normalize()])
+    transform_val = CustomCompose([ToTensor(), Resize(width, height), Normalize()])
 
     data_train = SegmentationDataset(args.data_dir, transform_train)
     n_classes = data_train.get_num_classes()
@@ -100,40 +115,42 @@ def train_model(args) -> None:
     loader_val = DataLoader(data_val, batch_size=args.batch_size, shuffle=False, drop_last=True, pin_memory=False)
 
     model = get_model(args, n_classes)
-    
+
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    weight = torch.cat([torch.ones((1, 3, width, height)).mul_(5.0), torch.ones((1, 3, width, height))], dim=1).to(device)
+    criterion = torch.nn.BCEWithLogitsLoss(weight=weight)
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, betas=(args.momentum, args.alpha),
                                   weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
-    # fp16_scaler = torch.cuda.amp.GradScaler() if args.use_fp16 and torch.cuda.is_available() else None
+    fp16_scaler = torch.cuda.amp.GradScaler() if args.use_fp16 and torch.cuda.is_available() else None
 
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     model.to(device)
 
     metrics = dict()
     for epoch in range(1, args.n_epochs +1):
         print(f'Epoch[{epoch:>3d}/{args.n_epochs:>3d}]:')
-        train_one_epoch(model, optimizer, scheduler, loader_train, device, writer, epoch)
-        if epoch % 1 == 0:
-            metrics = eval_one_epoch(model, loader_val, device, writer, epoch)
+        train_one_epoch(model, criterion, optimizer, scheduler, loader_train, fp16_scaler, device, writer, epoch)
+        if epoch % 5 == 0 or epoch == args.n_epochs:
+            metrics = eval_one_epoch(model, criterion, loader_val, device, writer, epoch)
+            torch.save(model.state_dict(), os.path.join(ckp_dir, f'{args.arch}_{epoch}.pth'))
 
     writer.add_hparams(hparam_dict=args.__dict__, metric_dict=metrics)
-    torch.save(model.state_dict(), os.path.join(ckp_dir, f'{args.arch}.pth'))
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='data/v3')
+    parser.add_argument('--data_dir', type=str, default='data/v4')
     parser.add_argument('--ckp_dir', type=str, default='checkpoints')
     parser.add_argument('--arch', type=str, default='deeplab',
                         choices=['deeplab', 'lraspp'])
 
     # Trainingsparameter
-    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--learning_rate', type=float, default=1e-2)
     parser.add_argument('--momentum', type=float, default=.9)
     parser.add_argument('--alpha', type=float, default=.99)
     parser.add_argument('--weight_decay', type=float, default=.0)
     parser.add_argument('--step_size', type=int, default=1)
-    parser.add_argument('--gamma', type=int, default=0.9)
+    parser.add_argument('--gamma', type=float, default=0.9)
     parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--n_epochs', type=int, default=10)
 
