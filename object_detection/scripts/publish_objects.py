283217bdb8909c888625f2f1ae212ec3d0194f3c
ManuelHeins <mah6220@thi.de>
2022-06-11 13:50:56 +0200

    Implemented calculating object positions

--- object_detection/scripts/publish_objects.py
+++ object_detection/scripts/publish_objects.py
@@ -1,31 +1,65 @@
 #!/usr/bin/env python
+import math
+import sys
 from camera_reader import CIBagReader, ImageListener, RealsenseReader
 from topics import CITopic
 import rospy
 from object_detection.msg import ObjectPosition, ObjectPositionArray
 from multiprocessing.connection import Client
+import cv2
+import pyrealsense2 as rs
 
 # region: testing
 
+COLORS = {
+    'iron_man': (255, 0, 0),
+    'hulk': (0, 255, 0),
+    'captain_america': (0, 0, 255),
+    'obstacles': (0, 0, 0),
+    'wall': (100, 100, 100)
+}
+
 class TestListener(ImageListener):
 
-    def __init__(self, object_publisher):
+    def __init__(self, object_publisher, skip_frames = 0):
         super(TestListener, self).__init__(CITopic.color_img.value)
         self.client = Client(('localhost', 6000))
         self.object_publisher = object_publisher
+        self.client.send('turtlebot')
+        self.skip_frames = skip_frames
+        self.skipped = 0
 
     def handle_img(self, np_image, timestamp):
+        if self.skipped < self.skip_frames:
+            self.skipped += 1
+            return
         super(TestListener, self).handle_img(np_image, timestamp)
         self.client.send(np_image)
-        object_dict = self.client.recv()
+        object_list = self.client.recv()
+
+        img = np_image
+
+        height = len(np_image)
+        width = 0
+        if height > 0:
+            width = len(np_image[0])
+
         detected_objects = []
-        for obj_id, pos in object_dict.items():
-            distance = 10 # TODO: calculate distance
-            angle = 0 # TODO: calculate angle
+        for obj_id, pos in object_list:
+            distance = 10 # test value
+            angle = 0 # test value
+            color = COLORS.get(obj_id, (0, 255, 255))
+            img = cv2.circle(img, (int(pos[0] * width), int(pos[1] * height)), 5, color, thickness=-1)
             detected_objects.append(ObjectPosition(obj_id, distance, angle))
 
         if self.object_publisher is not None:
             self.object_publisher.publish(ObjectPositionArray(detected_objects))
+
+        cv2.imshow('objects', img)
+        # cv2.waitKey(0)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            sys.exit(0)
 
 # endregion
 
@@ -35,15 +69,23 @@ class ObjectDetection(RealsenseReader):
         super(ObjectDetection, self).__init__(width, height, fps, None, None)
         self.client = Client(server_address)
         self.object_publisher = object_publisher
+        self.client.send('turtlebot')
 
     def handle_images(self, color_image, depth_image, color_timestamp, depth_timestamp, depth_colormap, color_intrin):
         super(ObjectDetection, self).handle_images(color_image, depth_image, color_timestamp, depth_timestamp, depth_colormap, color_intrin)
         self.client.send(color_image)
-        object_dict = self.client.recv()
+        object_list = self.client.recv()
         detected_objects = []
-        for obj_id, pos in object_dict.items():
-            distance = 10 # TODO: calculate distance
-            angle = 0 # TODO: calculate angle
+        d_height = len(depth_image)
+        d_width = 0
+        if d_height > 0:
+            d_width = len(depth_image[0])
+        for obj_id, pos in object_list:
+            x = pos[0] * d_width
+            y = pos[1] * d_height
+            distance = depth_image[y, x]
+            u, _, _ = rs.rs2_deproject_pixel_to_point(color_intrin, (x, y), distance)
+            angle = math.asin(u / distance)
             detected_objects.append(ObjectPosition(obj_id, distance, angle))
 
         if self.object_publisher is not None:
@@ -54,7 +96,7 @@ if __name__ == '__main__':
     rospy.init_node('object_detection', anonymous=True)
     object_publisher = rospy.Publisher('/detected_objects', ObjectPositionArray, queue_size=10)
 
-    # listener = TestListener(object_publisher)
+    # listener = TestListener(object_publisher, 2500)
     # object_detection = CIBagReader('/home/aimotion/TurtleBot/record3.bag', [listener])
     object_detection = ObjectDetection(object_publisher=object_publisher)
     
--- segmentation/server.py
+++ segmentation/server.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python3
+from argparse import Namespace
 from multiprocessing.connection import Listener, Connection
 from multiprocessing.reduction import ForkingPickler
 import threading
 
+import torch
+from augmentations import PreProcess
+from inference import extract_figures, extract_objects
+
+from models import load_pretrained_model
+import cv2
+
+WEIGHTS_DIR = '/home/aimotion/TurtleBot/TurtleBot/weights/%s/lraspp_30.pth'
+
 def handle_client(conn: Connection):
+
+    mode = conn.recv()
+
+    if mode == 'turtlebot':
+        obj_classes = ['iron_man', 'captain_america', 'hulk', 'free_space', 'obstacles', 'wall']
+    elif mode == 'topdown':
+        obj_classes = ['iron_man', 'captain_america', 'hulk', 'free_space', 'obstacles', 'wall', 'turtlebot', 'background']
+    else:
+        print('Unknown mode: %s' % str(mode))
+        conn.close()
+
+    print("Initializing mode '%s'" % mode)
+
+    args = Namespace(
+        mode=mode,
+        weights_dir=WEIGHTS_DIR % mode,
+        arch='lraspp',
+        obj_classes=obj_classes
+    )
+
+    model = load_pretrained_model(args)
+    model.eval()
+    preprocess = PreProcess()
     try:
         while True:
             # image = conn.recv()
@@ -13,12 +46,19 @@ def handle_client(conn: Connection):
             buf = conn._recv_bytes()
             image = ForkingPickler.loads(buf.getbuffer(), encoding='bytes')
             
-            # TODO: Get object positions from image
+            # Preprocess RGB-Frame
+            rgb_tensor = preprocess(image)
 
-            objects = {
-                'obstacle_0': (100, 200),
-                'hulk': (300, 150)
-            }
+            # Make Prediction
+            heatmap = model(rgb_tensor)['out']
+            idx = torch.argmax(heatmap, dim=1, keepdim=True)
+            prediction = torch.zeros_like(heatmap).scatter_(1, idx, 1.).squeeze()
+
+            # Extract detected Objects from Prediction
+            obj_labels, bboxes, t_centers = extract_figures(prediction)
+
+            objects = [(args.obj_classes[obj], (pos[0] / preprocess.width, pos[1] / preprocess.height))
+                        for (obj, pos) in zip(obj_labels, t_centers)]
 
             # conn.send(objects)
             # Workaround for python2 comppatibility
