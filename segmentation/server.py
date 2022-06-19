de52c7692e9ae79d0cecde305ed0d22ff4c42d2e
ManuelHeins <mah6220@thi.de>
2022-06-19 10:00:25 +0200

    Use fixed dimensions for figures and turtlebot

--- segmentation/server.py
+++ segmentation/server.py
@@ -30,7 +30,8 @@ def handle_client(conn: Connection):
 
     mode = conn.recv()
 
-    max_objects = [1, 1, 1, 0, 0, 0, 1, 0]
+    max_object_counts = [1, 1, 1, None, None, None, 1, None]
+    fixed_dimensions = [(0.07, 0.07), (0.07, 0.07), (0.07, 0.07), None, None, None, (0.1, 0.1), None]
 
     if mode == 'turtlebot':
         obj_classes = ['iron_man', 'captain_america', 'hulk', 'free_space', 'obstacles', 'wall']
@@ -55,7 +56,7 @@ def handle_client(conn: Connection):
     try:
         while True:
             # image = conn.recv()
-            # Workaround for python2 comppatibility
+            # Workaround for python2 compatibility
             conn._check_closed()
             conn._check_readable()
             buf = conn._recv_bytes()
@@ -79,27 +80,31 @@ def handle_client(conn: Connection):
                 objects = []
 
                 for obj_id, hull_list in zip(obj_labels, hulls):
-                    max_object_count = max_objects[obj_id]
+
+                    max_object_count = max_object_counts[obj_id]
+                    fixed_dim = fixed_dimensions[obj_id]
 
                     bounding_rects = []
                     for hull in hull_list:
                         rect = get_normalized_bounding_rect(hull, preprocess.width, preprocess.height)
                         bounding_rects.append(rect)
 
-                    if max_object_count > 0 and len(bounding_rects) > max_object_count:
-                        bounding_rects.sort(key=get_area, reverse=True)
-
                     object_count = len(bounding_rects)
-                    if max_object_count > 0:
-                        object_count = min(max_object_count, object_count)
+
+                    if max_object_count is not None and object_count > max_object_count:
+                        bounding_rects.sort(key=get_area, reverse=True)
+                        object_count = max_object_count
 
                     for i in range(object_count):
-                        rect = bounding_rects[i]
-                        obj_data = (obj_classes[obj_id], *rect)
+                        pos, dim, angle = bounding_rects[i]
+                        if fixed_dim is not None:
+                            dim = fixed_dim
+                            angle = 0
+                        obj_data = (obj_classes[obj_id], pos, dim, angle)
                         objects.append(obj_data)
 
             # conn.send(objects)
-            # Workaround for python2 comppatibility
+            # Workaround for python2 compatibility
             conn._check_closed()
             conn._check_writable()
             conn._send_bytes(ForkingPickler.dumps(objects, protocol=2))
